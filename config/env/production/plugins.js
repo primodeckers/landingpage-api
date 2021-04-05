6f2a908a3ecf5f430679b454d6481dcf9fc3e90a
Rene Estevam Deckers <rene.deckers@planejamento.gov.br>
2021-04-05 19:45:58 -0300

    Updated config heroku

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     providerOptions: {
       accessKeyId: env('AWS_ACCESS_KEY_ID'),
       secretAccessKey: env('AWS_ACCESS_SECRET'),
-      region: 'aws-region',
+      region: 'sa-east-1',
       params: {
-        Bucket: 'my-bucket',
+        Bucket: 'primodeckers',
       },
     },
   },
